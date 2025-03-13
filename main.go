7aa4a7027a6675d759bae943becdc2d073b4b422
Daniel Reuter <daniel@steadybit.com>
2025-03-13 09:34:36 +0100

    chore: remove `net/http/pprof` imports, now via extension-kit

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ import (
 	"github.com/steadybit/extension-prometheus/extinstance"
 	"github.com/steadybit/extension-prometheus/extmetric"
 	_ "go.uber.org/automaxprocs" // Importing automaxprocs automatically adjusts GOMAXPROCS.
-	_ "net/http/pprof"           //allow pprof
 )
 
 func main() {
